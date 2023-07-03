bca8466621db52c8b1450533ae8fc8b5cee924f8
guscaxile <gustavocaxile@outlook.com>
2023-07-03 18:41:29 -0300

    Adding get by id method

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -2,12 +2,7 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/customer")
@@ -16,17 +11,22 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): MutableList<CustomerModel> {
+    fun getAll(): MutableList<CustomerModel> {
         return customers
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody customer: CustomerModel){
-        val id = if (customers.isEmpty()){
+    fun create(@RequestBody customer: CustomerModel) {
+        val id = if (customers.isEmpty()) {
             1
-        }else{
-            customers.last().id + 1
+        } else {
+            customers.last().id.toInt() + 1
         }.toString()
 
         customers.add(CustomerModel(id, customer.name, customer.email))
