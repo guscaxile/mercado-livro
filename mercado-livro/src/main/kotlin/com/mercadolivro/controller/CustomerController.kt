ab80612842d99aee4d0ffb75472d13d3707343c7
guscaxile <gustavocaxile@outlook.com>
2023-07-03 18:28:56 -0300

    Adding HttpStatus response, and making endpoint create accept dynamic data

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,23 +1,36 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/customer")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel{
-        return CustomerModel("1", "Gustavo", "email@email.com")
+    fun getCustomer(): MutableList<CustomerModel> {
+        return customers
     }
 
     @PostMapping
-    fun create(@RequestBody customer: CustomerModel): CustomerModel{
-        return customer;
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: CustomerModel){
+        val id = if (customers.isEmpty()){
+            1
+        }else{
+            customers.last().id + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
+
+
 }
