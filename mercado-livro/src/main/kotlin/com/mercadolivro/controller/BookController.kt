4ca984f4b8c8df9f6f9d3bb3bf74500ea6876c3b
guscaxile <gustavocaxile@outlook.com>
2023-07-06 17:06:10 -0300

    all endpoints added and configured

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,17 +1,13 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.extension.toBookModel
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("book")
@@ -35,5 +31,23 @@ class BookController(
     @GetMapping("/active")
     fun findActives(): List<BookModel>{
         return bookService.findActives()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel{
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int){
+        bookService.delete(id)
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest){
+        val bookSaved = bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 }
--- mercado-livro/src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -20,4 +20,18 @@ class BookService(
     fun findActives(): List<BookModel> {
         return bookRepository.findByStatus(BookStatus.ATIVO)
     }
+
+    fun findById(id: Int): BookModel{
+        return bookRepository.findById(id).orElseThrow()
+    }
+
+    fun delete(id: Int) {
+        val book = findById(id)
+        book.status = BookStatus.CANCELADO
+        update(book)
+    }
+
+    fun update(book: BookModel) {
+        bookRepository.save(book)
+    }
 }
