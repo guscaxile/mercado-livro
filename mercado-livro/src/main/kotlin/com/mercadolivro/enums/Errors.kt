158416b892d58f4e698e0aeea5481fb1bfc833a9
guscaxile <gustavocaxile@outlook.com>
2023-07-07 20:55:36 -0300

    creating bad request exception

--- mercado-livro/src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -2,5 +2,6 @@ package com.mercadolivro.enums
 
 enum class Errors(val code: String, val message: String) {
     ML101("ML-101", "Book [%s] not exists"),
+    ML102("ML-102", "Cannot update book with status [%s]"),
     ML201("ML-201", "Customer [%s] not exists")
 }
--- mercado-livro/src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest
 class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException::class)
-    fun handlerException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse>{
+    fun handleNotFoundException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse>{
         val erro = ErrorResponse(
             HttpStatus.NOT_FOUND.value(),
             ex.message,
@@ -20,5 +20,17 @@ class ControllerAdvice {
         )
 
         return ResponseEntity(erro, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(BadRequestException::class)
+    fun handleBadRequestException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse>{
+        val erro = ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            ex.message,
+            ex.errorCode,
+            null
+        )
+
+        return ResponseEntity(erro, HttpStatus.BAD_REQUEST)
     }
 }
