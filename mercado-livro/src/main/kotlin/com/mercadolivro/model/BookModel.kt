18655386a36d518604cb84be26e542c86dd37a68
guscaxile <gustavocaxile@outlook.com>
2023-07-07 17:04:18 -0300

    blocking status change of canceled or blocked books

--- mercado-livro/src/main/kotlin/com/mercadolivro/model/BookModel.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/model/BookModel.kt
@@ -6,7 +6,7 @@ import javax.persistence.*
 
 @Entity(name = "book")
 data class BookModel(
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int? = null,
@@ -16,12 +16,27 @@ data class BookModel(
 
     @Column
     var price: BigDecimal,
-    
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
 
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null
-)
+) {
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.CANCELADO || field == BookStatus.DELETADO)
+                throw Exception("Não é possível alterar um livro com status $field")
+            field = value
+        }
+
+    constructor(
+        id: Int? = null,
+        name: String,
+        price: BigDecimal,
+        customer: CustomerModel? = null,
+        status: BookStatus?
+    ): this(id, name, price, customer) {
+        this.status = status
+    }
+}
