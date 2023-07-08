08cae16c6d552bf92f6dc8612cff015d409517ee
guscaxile <gustavocaxile@outlook.com>
2023-07-08 12:52:00 -0300

    first request validations with spring validator

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,6 +1,13 @@
 package com.mercadolivro.controller.request
 
-data class PostCustomerRequest (
-        var name: String,
-        var email: String
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
+data class PostCustomerRequest(
+
+    @field:NotEmpty
+    var name: String,
+
+    @field:Email
+    var email: String
 )
