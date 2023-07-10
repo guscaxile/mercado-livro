b5df583ec8e2952810e786d26509ffdf536bd00f
guscaxile <gustavocaxile@outlook.com>
2023-07-10 10:38:40 -0300

    setting up and firing first event

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/repository/PurchaseRepository.kt
@@ -0,0 +1,7 @@
+package com.mercadolivro.repository
+
+import com.mercadolivro.model.PurchaseModel
+import org.springframework.data.repository.CrudRepository
+
+interface PurchaseRepository : CrudRepository<PurchaseModel, Int> {
+}
