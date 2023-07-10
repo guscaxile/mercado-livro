babfad675abdccde89c3f0ac7c51b657a68cb709
guscaxile <gustavocaxile@outlook.com>
2023-07-10 15:49:09 -0300

    adding password on customer

--- mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -12,11 +12,22 @@ import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
-    return CustomerModel(name = this.name, email = this.email, status = CustomerStatus.ATIVO)
+    return CustomerModel(
+        name = this.name,
+        email = this.email,
+        status = CustomerStatus.ATIVO,
+        password = this.password
+    )
 }
 
 fun PutCustomerRequest.toCustomerModel(previousValue: CustomerModel): CustomerModel {
-    return CustomerModel(id = previousValue.id, name = this.name, email = this.email, status = previousValue.status)
+    return CustomerModel(
+        id = previousValue.id,
+        name = this.name,
+        email = this.email,
+        status = previousValue.status,
+        password = previousValue.password
+    )
 }
 
 fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel{
